e1565e37b4a64ad863d824764fbb6a80f3613343
KavinduNilshan <kavindunilshanliyanage@gmail.com>
2023-12-29 20:44:33 +0530

    Alert removed

--- src/services/onosServices.js
+++ src/services/onosServices.js
@@ -69,6 +69,5 @@ export const getDevices = async () => {
     
     } catch {
         console.log("Can not connect to the onos server");
-        alert("Can not connect to the onos server");
     }
 };
